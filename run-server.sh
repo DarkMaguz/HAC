b8bfce8902b23cc7f61b2213c9fdd0f0b76b555f
Peter Balling <peter@knowmio.com>
2018-02-25 14:17:14 +0100

    Added docker volume for MySQL data

--- run-server.sh
+++ run-server.sh
@@ -12,7 +12,7 @@ docker rm -f hacdb myadmin hac-server smtp
 docker build -t darkmagus/hac-server .
 
 # Launch services.
-docker run --name hacdb -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=hac01 -e MYSQL_USER=${MYSQL_USER} -e MYSQL_PASSWORD=${MYSQL_PASSWORD} -v $(pwd)/sql:/docker-entrypoint-initdb.d mysql/mysql-server:5.7
+docker run --name hacdb -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=hac01 -e MYSQL_USER=${MYSQL_USER} -e MYSQL_PASSWORD=${MYSQL_PASSWORD} -v $(pwd)/sql:/docker-entrypoint-initdb.d -v $(pwd)/datadir:/var/lib/mysql mysql/mysql-server:5.7
 docker run --name myadmin -d -p 8081:80 -e MYSQL_ROOT_PASSWORD=root --link hacdb:db phpmyadmin/phpmyadmin:4.7
 docker run --name smtp -d -p 25:25 -e RELAY_NETWORKS=:172.17.0.0/24 -e GMAIL_USER=${GMAIL_USER} -e GMAIL_PASSWORD=${GMAIL_PASSWORD} -e DISABLE_IPV6=true namshi/smtp:latest
 
