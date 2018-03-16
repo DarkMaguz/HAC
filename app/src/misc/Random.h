9ab3632ad4d5ff6cd5ff4c160ed18b5f0e57e835
Peter Balling <peter@knowmio.com>
2018-03-17 00:41:26 +0100

    Refactoring: Cleanup of Random class

--- app/src/misc/Random.h
+++ app/src/misc/Random.h
@@ -11,97 +11,51 @@
 #include <cstdio>
 using namespace std;
 
-template <class Type>
-void GetRnd( Type &rand )
+template<class Type>
+void GetRnd(Type &rand)
 {
 	FILE* rand_src;
-	
-	rand_src = fopen( "/dev/urandom", "r" );
-	
-	fread( &rand, sizeof(Type), 1, rand_src );
-	
-	fclose( rand_src );
-	
+	rand_src = fopen("/dev/urandom", "r");
+	fread(&rand, sizeof(Type), 1, rand_src);
+	fclose(rand_src);
 }
 
-inline void GetRndStr( void *rand, size_t size )
+inline void GetRndStr(void *rand, size_t size)
 {
-	
 	FILE* rand_src;
-	
-	rand_src = fopen( "/dev/urandom", "r" );
-	
-	fread( rand, 1, size, rand_src );
-	
-	fclose( rand_src );
-	
+	rand_src = fopen("/dev/urandom", "r");
+	fread(rand, 1, size, rand_src);
+	fclose(rand_src);
 }
 
-/*template <class Type>
-void GetRndStr( Type *rand, size_t size )
-{
-	
-	FILE* rand_src;
-	
-	rand_src = fopen( "/dev/urandom", "r" );
-	
-	fread( &rand, sizeof(Type), size, rand_src );
-	
-	fclose( rand_src );
-	
-}*/
-
-template <class Type>
+template<class Type>
 class Random
 {
 	public:
-		Random( unsigned long long i ) :
-			it( i )
+		Random(unsigned long long i) :
+				it(i)
 		{
-			
 			r = new Type[it];
-			
-			rand_src = fopen( "/dev/urandom", "r" );
-			
-			fread( r, sizeof(Type), it, rand_src );
-			
-			fclose( rand_src );
-			
+			rand_src = fopen("/dev/urandom", "r");
+			fread(r, sizeof(Type), it, rand_src);
+			fclose(rand_src);
 		}
 		
 		virtual ~Random()
 		{
-			delete []r;
+			delete[] r;
 		}
 		
-		inline Type Rnd( Type n )
+		inline Type Rnd(Type n)
 		{
-			
 			it--;
-			
-			return ( r[it] % (n+1) );
-			
+			return (r[it] % (n + 1));
 		}
-		
-		/*inline Type Rnd2( void )
-		{
-			
-			it--;
-			
-			return r[it];
-			
-		}*/
-		
-	protected:
 		
 	private:
-		
 		FILE* rand_src;
-		
 		unsigned long long it;
-		
 		Type *r;
-		
 };
 
 #endif /* RANDOM_H_ */
